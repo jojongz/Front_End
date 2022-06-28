7b3146a13c0516f059518b12ffd5967be60085a6
DESKTOP-JDC32PP\ez220410 <whdcjs8850@naver.com>
2022-06-28 18:44:14 +0900

    0624

---
+++ 0624/test04.js
@@ -0,0 +1,16 @@
+// 삼항 연산자 (조건연산자)
+let condition = 5>10;
+(condition)? console.log('left'):console.log('right');
+
+let result = condition?(
+    console.log("삼항연산자 참값 표현   "),
+    "표현식1"
+
+
+):(
+    console.log("삼항연산자 거짓값 표현   "),
+    "표현식2"
+
+
+);
+console.log(result);
---
+++ 0624/test07.js
@@ -0,0 +1,6 @@
+// 배열
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.length);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[8]);
